b2437ac6a91fe7963fa4b7f25e2fad26977e8051
Neeraj Rajput <92433406+neerajkumar790652@users.noreply.github.com>
2023-08-10 19:34:51 +0530

    Update AboutInfo.js

--- src/component/AboutInfo.js
+++ src/component/AboutInfo.js
@@ -3,7 +3,7 @@ const AboutInfo = ()=>{
     return(
         <>
         <h1>Bitto Aggarwal</h1>
-        <h5>Software Developer,Educator at Geekster</h5>
+        <h5>Software Developer ,Educator at Geekster</h5>
         < a href="/" target="_blank">Linkedin</a> &ensp;
         <a href="/" target="_blank">Github</a>
         <hr/>
